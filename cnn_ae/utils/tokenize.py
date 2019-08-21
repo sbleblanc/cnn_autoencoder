22fb7d23f5ca3688235f848742a865a3f0f1e048
Samuel Beland-Leblanc <samuel.beland.leblanc@gmail.com>
2019-08-21 13:55:58 -0400

    Added WordToCharTokenizer

--- cnn_ae/utils/tokenize.py
+++ cnn_ae/utils/tokenize.py
@@ -1,4 +1,4 @@
-
+import re
 
 def tokenize_to_char(string):
     char_string = []
@@ -8,3 +8,17 @@ def tokenize_to_char(string):
     if not string.endswith(' '):
         char_string.pop(-1)
     return char_string
+
+
+class WordToCharTokenizer(object):
+
+    def __init__(self, valid_content_regex=r"[a-z\s]+"):
+        self.extract_re = re.compile(valid_content_regex)
+
+    def __call__(self, string):
+        char_string = []
+        valid_parts = self.extract_re.findall(string.replace("\n", " "))
+        for w in valid_parts:
+            char_string.extend([c for c in w])
+        char_string.append(' ')
+        return char_string
